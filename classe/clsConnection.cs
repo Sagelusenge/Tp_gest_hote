43fd103e77c5261c71f0188c5b3a60963abb391a
Sagelusenge <159254397+Sagelusenge@users.noreply.github.com>
2025-07-10 17:22:28 +0200

    update

--- classe/clsConnection.cs
+++ classe/clsConnection.cs
@@ -13,7 +13,11 @@ namespace ChambreHotel.classe
 
         public void sage()
         {
-
+            int a = 0;
+            for(int i = 0; i < 10; i++)
+            {
+                a += i;
+            }
         }
     }
 
